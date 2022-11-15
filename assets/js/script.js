56eedd7866fa8c137000ecd8d337142b6e128667
khinoj <kristopher.hinojosa@gmail.com>
2022-11-14 21:32:29 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// <reference path=path/to/jqtypes/for/autosuggestions />
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -6,39 +9,34 @@
 // currentTime = now.format('H:mm:ss');
 // $('#currentDay').text(currentDate);
 
-let date = dayjs().format('dddd-MMM-YYYY HH:mm:ss');
-
-console.log('test', date);
 
 // setInterval(() => console.log(new Date().toLocaleTimeString(),1000));
-
-$('#currentDay').text(date);
-
-
 
 var todayTask = [];
 
 
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+$(function () {
+  let date = dayjs().format('dddd-MMM-YYYY HH:mm:ss');
+  $('#currentDay').text(date);
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
+  // var timeBlockElms = $('.time-block')
+  console.log($('.time-block'));
+  function handleTimeBlockChange() {
+    console.log('handlingtimeblockchange', date);
+    setTimeout(() => {
+      if ($('.time-block').attr("#hour-" + 12) == date) {
+        $('.time-block').removeClass('future')
+        $('.time-block').addClass('present')
+      }
+
+    }, 3000);
+  }
+
+  $(window).on("load", function () {
+    console.log('document loaded', event);
+    handleTimeBlockChange();
+  })
+});
 
 var todayTask = { 'one': 1, 'two': 2, 'three': 3 };
 
